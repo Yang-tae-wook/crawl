b5b108667cffd55317f413660a62bcfb905f9b98
Yang-tae-wook <wooke1995@gmail.com>
2018-02-09 15:45:24 +0900

    thumb, one, bit, finex

--- abc.py
+++ abc.py
@@ -18,14 +18,15 @@ class AsyncTask:
         Name = ['Timestamp', 'BTC', 'ETH', 'XRP', 'BCH']
         f1 = open('bithumb_balnce.csv', 'a', encoding='utf-8')
         f2 = open('coinone_balance.csv', 'a', encoding='utf-8')
-#        f3 = open('korbit_balance.csv', 'a', encoding='utf-8')
+        f3 = open('korbit_balance.csv', 'a', encoding='utf-8')
         f4 = open('bitfinex_balance.csv', 'a', encoding='utf-8')
 
         wr1 = csv.writer(f1)
         wr1.writerow(Name)
         wr2 = csv.writer(f2)
         wr2.writerow(Name)
-#        wr3 = csv.writer(f3)
+        wr3 = csv.writer(f3)
+        wr3.writerow(Name)
         wr4 = csv.writer(f4)
         wr4.writerow(Name)
 
@@ -48,15 +49,38 @@ class AsyncTask:
 
             wr2.writerow([Time_one, BTC_one, ETH_one, XRP_one, BCH_one])
 
-            '''
-            Time_bit = int( json.loads(urllib.request.urlopen('https://api.korbit.co.kr/v1/ticker?currency_pair=btc_krw').read())['timestamp'])
-            BTC_bit = int( json.loads(urllib.request.urlopen('https://api.korbit.co.kr/v1/ticker?currency_pair=btc_krw').read())['last'] )
-            ETH_bit = int( json.loads(urllib.request.urlopen('https://api.korbit.co.kr/v1/ticker?currency_pair=eth_krw').read())['last'] )
-            XRP_bit = int( json.loads(urllib.request.urlopen('https://api.korbit.co.kr/v1/ticker?currency_pair=xrp_krw').read())['last'] )
-            BCH_bit = int( json.loads(urllib.request.urlopen('https://api.korbit.co.kr/v1/ticker?currency_pair=bch_krw').read())['last'] )
-            wr3.writerow(Name)
+
+
+            Time_bit = round(float(json.loads(urllib.request.urlopen('https://api.bithumb.com/public/ticker/all').read())['data']['date']))
+
+            reqBTC = Request('https://api.korbit.co.kr/v1/ticker?currency_pair=btc_krw' , headers={'User-Agent': 'Mozilla/5.0'})
+            readBTC = urlopen(reqBTC).read()
+            jsonBTC = json.loads(readBTC)
+            FindBTC = jsonBTC['last']
+            BTC_bit = int(FindBTC)
+
+            reqETH = Request('https://api.korbit.co.kr/v1/ticker?currency_pair=eth_krw' , headers={'User-Agent': 'Mozilla/5.0'})
+            readETH = urlopen(reqETH).read()
+            jsonETH = json.loads(readETH)
+            FindETH = jsonETH['last']
+            ETH_bit = int(FindETH)
+
+            reqXRP = Request('https://api.korbit.co.kr/v1/ticker?currency_pair=xrp_krw' , headers={'User-Agent': 'Mozilla/5.0'})
+            readXRP = urlopen(reqXRP).read()
+            jsonXRP = json.loads(readXRP)
+            FindXRP = jsonXRP['last']
+            XRP_bit = int(FindXRP)
+
+            reqBCH = Request('https://api.korbit.co.kr/v1/ticker?currency_pair=bch_krw' , headers={'User-Agent': 'Mozilla/5.0'})
+            readBCH = urlopen(reqBCH).read()
+            jsonBCH = json.loads(readBCH)
+            FindBCH = jsonBCH['last']
+            BCH_bit = int(FindBCH)
+
+
             wr3.writerow([Time_bit, BTC_bit, ETH_bit, XRP_bit, BCH_bit])
-            '''
+
+
 
             Time_finex = round(float(json.loads(urllib.request.urlopen('https://api.bitfinex.com/v1/pubticker/btcusd').read())['timestamp']))*1000
             BTC_finex = float(json.loads(urllib.request.urlopen('https://api.bitfinex.com/v1/pubticker/btcusd').read())['last_price'])
@@ -78,13 +102,13 @@ class AsyncTask:
             print("ETH: ", ETH_one)
             print("XRP: ", XRP_one)
             print("BCH: ", BCH_one)
-            '''
+
             print("Time: ", Time_bit)
             print("BTC: ", BTC_bit)
             print("ETH: ", ETH_bit)
             print("XRP: ", XRP_bit)
             print("BCH: ", BCH_bit)
-            '''
+
 
             print("Time: ", Time_finex)
             print("BTC: ", BTC_finex)
@@ -95,7 +119,7 @@ class AsyncTask:
             time.sleep(10)                       #몇초간격
         f1.close()
         f2.close()
-#        f3.close()
+        f3.close()
         f4.close()
 
 
