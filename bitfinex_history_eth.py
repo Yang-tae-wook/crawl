d17debcfa4aa705b223937750d427779a2659c93
Yang-tae-wook <wooke1995@gmail.com>
2018-02-12 16:57:20 +0900

    update all 2

---
+++ bitfinex_history_eth.py
@@ -0,0 +1,45 @@
+import requests
+import csv
+import threading
+import time
+import json
+import datetime
+
+class AsyncTask:
+    def __init__(self):
+        pass
+
+    def TaskA(self):
+        counter = 0
+        Name = ['Timestamp', 'OPEN', 'CLOSE', 'HIGH', 'LOW', 'VOLUMN']
+        f = open('bitfinex_hist.csv', 'w', encoding='utf-8')
+
+        wr = csv.writer(f)
+        wr.writerow(Name)
+
+        end = 1518412899000
+
+        while(end > 1458149797000):
+            url = 'https://api.bitfinex.com/v2/candles/trade:5m:tETHUSD/hist'
+            params = { 'start': end-35700000, 'end': end }
+            r = requests.get(url, params = params)
+            data = r.json()
+            print(data)
+            end = end - 35700000
+            for row in data:
+                #print(row)
+                wr.writerow(row)
+
+            time.sleep(5)
+        f.close()
+
+
+def main():
+    print('Async Function')
+
+    at = AsyncTask()
+    at.TaskA()
+
+
+if __name__ == '__main__':
+    main()
