08af0d0825f08772033ef31fb63525236f2fb482
Yang-tae-wook <wooke1995@gmail.com>
2018-02-28 14:09:41 +0900

    ethqtum

--- bitfinex_history_eth.py
+++ bitfinex_history_eth.py
@@ -4,33 +4,55 @@ import threading
 import time
 import json
 import datetime
+import numpy as np
 
 class AsyncTask:
     def __init__(self):
         pass
 
     def TaskA(self):
-        counter = 1
-        Name = ['Timestamp', 'OPEN', 'CLOSE', 'HIGH', 'LOW', 'VOLUMN']
-        f = open('bitfinex_hist_eth.csv', 'w', encoding='utf-8')
 
-        wr = csv.writer(f)
-        wr.writerow(Name)
+        Name = ['Timestamp', 'CLOSE']
+        f1 = open('bitfinex_hist_eth.csv', 'w', encoding='utf-8')
+        f2 = open('bitfinex_hist_qtum.csv', 'w', encoding='utf-8')
 
-        end = 1518412899000
+        wr1 = csv.writer(f1)
+        wr2 = csv.writer(f2)
+        wr1.writerow(Name)
+        wr2.writerow(Name)
 
-        while(end > 1458149797000):
-            url = 'https://api.bitfinex.com/v2/candles/trade:5m:tETHUSD/hist'
-            params = { 'start': end-35700000, 'end': end }
-            r = requests.get(url, params = params)
-            data = r.json()
-            print(counter)
-            end = end - 35700000
-            for row in data:
-                #print(row)
-                wr.writerow(row)
-            counter += 1
+        end = 1519788177000
+
+        while(end > 1506816000000):
+            coin_type=["ETH", "QTM"]
+
+            for i in coin_type:
+                url = "https://api.bitfinex.com/v2/candles/trade:5m:t"+i+"USD/hist"
+
+
+                params = { 'start': end-35700000, 'end': end }
+                r = requests.get(url, params = params)
+                data = r.json()
+                print(data)
+                end = end - 35700000
+
+                data_arr = np.array(data)
+
+                close_data = data_arr[:,0:3:2]
+                print(close_data)
+
+                for row in close_data:
+                    print(row)
+                    if i == "ETH":
+                        wr1.writerow(row)
+                    else:
+                        wr2.writerow(row)
+
+
+
+
             time.sleep(5)
+
         f.close()
 
 
